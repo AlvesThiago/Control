f713160d002a0a0f3a33678dcd8235ea269c9fe7
Thiago Alves (ADMIN) <thiagodesign.27@gmail.com>
2025-02-03 22:33:36 -0300

    teste 1 conexão

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,7 +5,7 @@ const drizzleConfig = {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DRIZZLE_DB_URL
+        url: 'postgresql://controldb_owner:ElM3sD5WcXLN@ep-autumn-darkness-a51sbhi0.us-east-2.aws.neon.tech/controldb?sslmode=require'
     }
 };
 
